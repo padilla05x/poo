e6fc5f2dc8581493a4f81a5a3ff001aa269b3290
Santiago Padilla <sanpari301@gmail.com>
2024-02-22 20:37:17 -0500

    adiciono la prueba

---
+++ veterinaria11/src/test/java/co/edu/uniquindio/poo/AppTest.java
@@ -0,0 +1,50 @@
+/**
+ * Clase para probar el funcionamiento del código
+ * @author Área de programación UQ
+ * @since 2023-08
+ * 
+ * Licencia GNU/GPL V3.0 (https://raw.githubusercontent.com/grid-uq/poo/main/LICENSE) 
+ */
+package co.edu.uniquindio.poo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.logging.Logger;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    private static final Logger LOG = Logger.getLogger(AppTest.class.getName());
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void datosCompletos() {
+        LOG.info("Iniciado test datos completos");
+        
+        Veterinaria veterinaria = new Veterinaria("Firulais", "especie", "raza", (byte) 12, "macho", "cafe", (byte) 10);
+
+        assertEquals("Firulais", veterinaria.nombre());
+        assertEquals("especie", veterinaria.especie());
+        assertEquals("raza", veterinaria.raza());
+        assertEquals((byte) 12, veterinaria.edad());
+        assertEquals("macho", veterinaria.genero());
+        assertEquals("cafe", veterinaria.color());
+        assertEquals((byte) 10, veterinaria.peso());
+
+
+
+
+
+
+    
+
+
+
+
+
+        LOG.info("Finalizando test datos completos");
+    }
+}
